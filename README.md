3dda1c905698089ad543857f65524bfcb2f4edba
J.T. Conklin <J.T.Conklin@Dell.Com>
2016-12-23 14:05:50 -0800

    Initial commit Copyright 2016, Dell Inc.
    From: fbe1d550d22496f8c6d79deb2635fb4a8fd10493

---
+++ README.md
@@ -0,0 +1,13 @@
+opx-db-sql
+==========
+opx-db-sql is wrapper to db-sql api used for VM target.
+
+Description
+-----------
+VM presents an abstraction of the hardware (thermal sensors, fans,
+LEDs, optics,interfaces, etc.) in a VM environment. SDI-VM's sole
+client is PAS, similarly SAI-VM client is NAS and it queries the
+devices using the API defined in sdi-api and functionality in sai-api.
+
+(c) Dell 2016
+
---
+++ inc/opx/db_sql_defines.h
@@ -0,0 +1,26 @@
+/*
+ * filename: db_sql_defines.h
+ * (c) Copyright 2014 Dell Inc. All Rights Reserved.
+ */
+
+/**
+ * \file db_sql_defines.h
+ * \brief   DB SQL constant Definitionss
+ */
+
+#ifndef _DB_SQL_DEFINES_H_
+#define _DB_SQL_DEFINES_H_
+
+/** Database table name length */
+#define DB_SQL_TABLE_NAME_LENGTH 32
+/** Maximum number of tables per Database */
+#define VM_MAX_TABLES_PER_DB 4
+/** Attribute Name Length */
+#define DB_SQL_ATTRIB_NAME_LENGTH 32
+/** Length of SQL Condition String */
+#define DB_SQL_CONDITION_STR_LEN 64
+/** Parameter String Length */
+#define DB_SQL_PARAMETER_STR_LEN 16
+
+#endif /* _DB_SQL_DEFINES_H_ */
+
